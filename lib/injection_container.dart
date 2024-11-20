ae1e423622028cb017cce72d8805fe09e0d3ffd6
basgomcesar <cesarbasiliogomez@gmail.com>
2024-11-20 16:46:58 -0600

    Cambio en la vista de los productos

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -24,7 +24,7 @@ void initInjections() {
 
   serviceLocator.registerLazySingleton<ClientChannel>(
     () => ClientChannel(
-      'localhost',
+      '192.168.100.40',
       port: 50051,
       options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
     )
